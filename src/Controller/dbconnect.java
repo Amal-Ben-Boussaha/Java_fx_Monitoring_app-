b8086020a50375c05026f1e4c63eac3620637afd
Amal-Ben-Boussaha <72451337+Amal-Ben-Boussaha@users.noreply.github.com>
2021-02-16 03:48:54 +0100

    Update dbconnect.java

--- src/Controller/dbconnect.java
+++ src/Controller/dbconnect.java
@@ -18,7 +18,7 @@ public Connection getConnection(){
      Connection conn = null;
         try{
             
-             conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/esen","root","");
+             conn = DriverManager.getConnection("","","");
             if(conn != null)
             {
                 System.out.println("connected to database");
